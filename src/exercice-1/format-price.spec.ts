b51c003345001c28f87ac53d4495835e2a45f4de
Mamourou SANGARE <ms284034@iut21.u-bourgogne.fr>
2024-06-18 12:32:21 +0200

    test unitaire niveau 1

--- src/exercice-1/format-price.spec.ts
+++ src/exercice-1/format-price.spec.ts
@@ -1,9 +1,15 @@
 
-import {test, it, describe} from 'vitest';
+
+
+const d = 56.6846515
+
+import {test, it, describe, expect} from 'vitest';
     describe("Doit retourner ", function(){
         test("Niveau 1", function(){
-            //expect()
+            expect(formatPrice(d) == d.toFixed(2).replace('.',',').toString().replace(/\B(?=(\d{3})+(?!\d))/g,  ' ' ).toString())
         }
+
+        // TextDecoderStream()
         
         );
 
