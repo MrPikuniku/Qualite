3711d36b8abbfe9e5153733bb5ab675f82958d7a
Mamourou SANGARE <ms284034@iut21.u-bourgogne.fr>
2024-06-18 11:55:48 +0200

    test de la foncton formatPrice()

---
+++ src/exercice-1/format-price.spec.ts
@@ -0,0 +1,19 @@
+
+import {test, it, describe} from 'vitest';
+    describe("Doit retourner ", function(){
+        test("Niveau 1", function(){
+            //expect()
+        }
+        
+        );
+
+
+
+
+
+    })
+
+
+
+
+
---
+++ src/exercice-1/format-price.ts
@@ -0,0 +1,18 @@
+function formatPrice(num: number, separator?:{"decimal": string, "milliers": string}){
+    if (isNaN(num)) {
+        throw new Error('Le paramètre doit être un nombre');
+    }
+    let arrondi = num.toFixed(2);
+    let virgule = arrondi.replace('.', separator?.decimal ? separator.decimal : ',');
+    let millier = virgule.toString().replace(/\B(?=(\d{3})+(?!\d))/g, separator?.milliers? separator.milliers: ' ');
+    let str:string = millier.toString();
+    return(str); 
+}
+
+
+console.log(formatPrice(17.844654165654568987911985));
+
+
+
+
+
